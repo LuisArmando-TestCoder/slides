74a1d358229316533cc0db12dd5191092e89961b
Luis Murillo <l.murillo.baltodano@accenture.com>
2021-01-27 12:59:11 -0600

    change exports

--- src/components/Molecule/index.ts
+++ src/components/Molecule/index.ts
@@ -1,9 +1,15 @@
+import Certificate from './Certificate'
 import CTACanvas from './CTACanvas'
+import Form from './Form'
 import Hint from './Hint'
+import Molecule from './Molecule'
 import Slide2D from './Slide2D'
 
 export {
+   Certificate,
    CTACanvas,
+   Form,
    Hint,
+   Molecule,
    Slide2D,
 }
