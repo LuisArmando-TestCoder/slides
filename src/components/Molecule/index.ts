a8ff1ff0e5992128458a8eb181e56e0dda0a27d0
Luis Murillo <l.murillo.baltodano@accenture.com>
2021-01-27 13:01:55 -0600

    change components to Atom

--- src/components/Molecule/index.ts
+++ src/components/Molecule/index.ts
@@ -1,15 +1,1 @@
-import Certificate from './Certificate'
-import CTACanvas from './CTACanvas'
-import Form from './Form'
-import Hint from './Hint'
-import Molecule from './Molecule'
-import Slide2D from './Slide2D'
-
-export {
-   Certificate,
-   CTACanvas,
-   Form,
-   Hint,
-   Molecule,
-   Slide2D,
-}
+export {}
